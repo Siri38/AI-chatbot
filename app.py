a6ac60da5bf48a44dff6f63a7224c7d484e882ec
Sirisha <133642953+Siri38@users.noreply.github.com>
2024-04-25 21:03:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import google.generativeai as genai
+import streamlit as st
+
+with open("keys/.geminiai_api_key.txt") as f:
+    key = f.read()
+genai.configure(api_key=key)
+
+st.balloons()
+st.title("🗨️🤖AI Conversational Chatbot")
+st.title("...Iam CHITTI the robot...")
+
+st.chat_message("ai").write("Hi, How can i help you today?")
+
+user_input = st.chat_input()
+
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+                              system_instruction="""You are a helpful AI Assistant who answers all the user queries politely.
+                              If someone asks your name, tell them that your name is Chitti the Robot.""")
+
+if "memory" not in st.session_state:
+    st.session_state["memory"] = []
+
+chat = model.start_chat(history=st.session_state['memory'])
+
+for message in chat.history:
+    st.chat_message(message.role).write(message.parts[0].text)
+
+if user_input:
+    st.chat_message("user").write(user_input)
+    response = chat.send_message(user_input)
+    st.chat_message("ai").write(response.text)
+    st.session_state["memory"] = chat.history
